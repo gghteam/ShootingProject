64cfbfa2af12589a708128451d9b077e323e05bd
채한승 <dmetinn@gmail.com>
2021-01-17 22:24:29 +0900

    기본 씬 세팅 추가

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.5f;
+    private MeshRenderer backgroundRenderer = null;
+
+    private Vector2 offset = Vector2.zero;
+    private void Start()
+    {
+        if(backgroundRenderer == null)
+        {
+            backgroundRenderer = gameObject.GetComponent<MeshRenderer>();
+        }
+    }
+
+    private void Update()
+    {
+        offset += new Vector2(0f, speed * Time.deltaTime);
+        backgroundRenderer.material.SetTextureOffset("_MainTex", offset);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private Animator playerAnimator = null;
+
+    private Vector3 currentPosition = Vector3.zero;
+    private Vector3 targetPosition = Vector3.zero;
+
+    private void Start()
+    {
+        playerAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) == true)
+        {
+            playerAnimator.SetBool("Attack", false);
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            currentPosition = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            transform.position = currentPosition;
+        }
+        else
+        {
+            playerAnimator.SetBool("Attack", true);
+        }
+    }
+}
